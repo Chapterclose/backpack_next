dc0e7e7ebbace6b0b823eeb83e3982bdeb0d0ff8
monibbormon <monibbormon14@gmail.com>
2025-08-06 19:38:53 +0600

    meta mask

--- components/Footer/FooterCopyright.jsx
+++ components/Footer/FooterCopyright.jsx
@@ -1,6 +1,6 @@
 const FooterCopyRight = () => {
   return (
-    <div className="text-center pt-5 pb-10 font-semibold border-t border-gray-100">
+    <div className="text-center pt-5 pb-10 dark:text-white dark:border-gray-500 font-semibold border-t border-gray-100">
       BackPack Exchange&copy; 2024{" "}
     </div>
   );
